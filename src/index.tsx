572642e605de849bb6eef34f1d36b591db6ff159
Louis Jordan <louisjordan@hotmail.com>
2019-12-28 15:44:46 +0000

    Adds StrictMode to app

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,14 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import App from "./app";
-import * as serviceWorker from "./serviceWorker";
+import React, { StrictMode } from 'react';
+import ReactDOM from 'react-dom';
+import App from './app';
+import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(
+    <StrictMode>
+        <App />
+    </StrictMode>,
+    document.getElementById('root')
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
