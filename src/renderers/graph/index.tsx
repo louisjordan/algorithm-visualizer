ca6525da03a123c2712ff19a35e4e4755590ab0f
Louis Jordan <louisjordan@hotmail.com>
2020-03-22 22:51:50 +0000

    Creates graph renderer

--- src/renderers/graph/index.tsx
+++ src/renderers/graph/index.tsx
@@ -1,87 +1,28 @@
 import React, { useState, useRef, useEffect } from 'react';
-import { Graph } from 'data-structures/graph';
-
 import { Graph as D3Graph } from 'react-d3-graph';
+import { baseD3Config, nodeConfig } from './configs';
 
-// graph payload (with minimalist structure)
+type GraphRendererNode = {
+    id: string;
+    label: string;
+    visited?: boolean;
+    active?: boolean;
+};
 
-// the graph configuration, you only need to pass down properties
-// that you want to override, otherwise default ones will be used
-
-// graph event callbacks
-// const onClickGraph = function() {
-//     window.alert(`Clicked the graph background`);
-// };
-
-// const onClickNode = function(nodeId) {
-//     window.alert(`Clicked node ${nodeId}`);
-// };
-
-// const onDoubleClickNode = function(nodeId) {
-//     window.alert(`Double clicked node ${nodeId}`);
-// };
-
-// const onRightClickNode = function(event, nodeId) {
-//     window.alert(`Right clicked node ${nodeId}`);
-// };
-
-// const onMouseOverNode = function(nodeId) {
-//     window.alert(`Mouse over node ${nodeId}`);
-// };
-
-// const onMouseOutNode = function(nodeId) {
-//     window.alert(`Mouse out node ${nodeId}`);
-// };
-
-// const onClickLink = function(source, target) {
-//     window.alert(`Clicked link between ${source} and ${target}`);
-// };
-
-// const onRightClickLink = function(event, source, target) {
-//     window.alert(`Right clicked link between ${source} and ${target}`);
-// };
-
-// const onMouseOverLink = function(source, target) {
-//     window.alert(`Mouse over in link between ${source} and ${target}`);
-// };
-
-// const onMouseOutLink = function(source, target) {
-//     window.alert(`Mouse out link between ${source} and ${target}`);
-// };
-
-// const onNodePositionChange = function(nodeId, x, y) {
-//     window.alert(
-//         `Node ${nodeId} is moved to new position. New position is x= ${x} y= ${y}`
-//     );
-// };
-
-const baseConfig = {
-    maxZoom: 1,
-    minZoom: 1,
-    d3: {
-        gravity: -1000,
-        linkLength: 50,
-    },
-    node: {
-        color: '#404040',
-        fontColor: '#bfbfbf',
-        fontSize: '1em',
-        size: 500,
-        strokeColor: '#333333',
-        strokeWidth: 3,
-    },
-    link: {
-        color: '#bfbfbf',
-        strokeWidth: 2,
-    },
+type GraphRendererLink = {
+    source: string;
+    target: string;
 };
 
 type Props = {
-    // graph: Graph<React.ReactNode>;
+    nodes: GraphRendererNode[];
+    links: GraphRendererLink[];
 };
+
 const GraphRenderer: React.FC<Props> = (props) => {
     const container = useRef<HTMLDivElement | null>(null);
-    const [config, setConfig] = useState({
+    const [d3Config, setD3Config] = useState({
+        ...baseD3Config,
         width: 400,
         height: 400,
     });
@@ -93,8 +34,8 @@ const GraphRenderer: React.FC<Props> = (props) => {
                 clientWidth: width,
             } = container.current;
 
-            setConfig((config) => ({
-                ...config,
+            setD3Config((d3Config) => ({
+                ...d3Config,
                 height,
                 width,
             }));
@@ -111,17 +52,23 @@ const GraphRenderer: React.FC<Props> = (props) => {
         };
     }, [container.current]);
 
-    const d3Config = {
-        ...baseConfig,
-        ...config,
-    };
-
     const data = {
-        nodes: [{ id: 'Harry' }, { id: 'Sally' }, { id: 'Alice' }],
-        links: [
-            { source: 'Harry', target: 'Sally' },
-            { source: 'Harry', target: 'Alice' },
-        ],
+        nodes: props.nodes.map((n) => {
+            const { visited, active, ...nodeValue } = n;
+
+            let node: any = { ...nodeValue }; // TODO: write / source declaration file for react-d3-array + remove any type
+
+            if (visited) {
+                node = { ...node, ...nodeConfig.highlight };
+            }
+
+            if (active) {
+                node = { ...node, ...nodeConfig.active };
+            }
+
+            return node;
+        }),
+        links: props.links,
     };
 
     return (
@@ -131,17 +78,6 @@ const GraphRenderer: React.FC<Props> = (props) => {
                 style={{ display: 'block', margin: '0 auto' }}
                 data={data}
                 config={d3Config}
-                // onClickNode={onClickNode}
-                // onDoubleClickNode={onDoubleClickNode}
-                // onRightClickNode={onRightClickNode}
-                // onClickGraph={onClickGraph}
-                // onClickLink={onClickLink}
-                // onRightClickLink={onRightClickLink}
-                // onMouseOverNode={onMouseOverNode}
-                // onMouseOutNode={onMouseOutNode}
-                // onMouseOverLink={onMouseOverLink}
-                // onMouseOutLink={onMouseOutLink}
-                // onNodePositionChange={onNodePositionChange}
             />
         </div>
     );
